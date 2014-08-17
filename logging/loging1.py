a79ac27f70cb912bc966ad402dc468563826b595
liziqiang <lzq.hdu@gmail.com>
2014-08-18 01:55:41 +0800

    logging

--- logging/loging1.py
+++ logging/loging1.py
@@ -8,14 +8,18 @@ logging.basicConfig(filename='example.log', filemode='w', level=logging.INFO)
 logger = logging.getLogger(__name__)
 effect_lvl = logger.getEffectiveLevel()
 print logging.getLevelName(effect_lvl)
+logger.setLevel(logging.DEBUG)
 
 #disable logging
 logging.disable(logging.CRITICAL)
 logging.critical('This message should not go to the log file')
 logging.info('So neither this')
+logger.info('So neither this')
 
 #restore logging
 logging.disable(logging.NOTSET)
 logging.debug('This message should not go to the log file...')
+logger.debug('This message should go to the log file...')
 logging.info('This message should go to the log file')
+logger.info('This message should go to the log file')
 logging.warning('And this, too')
