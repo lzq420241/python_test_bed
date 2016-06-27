c5d42db964ccc15949f1c605bb04e83ecbbbf48d
liziqiang <lzq.hdu@gmail.com>
2016-06-28 00:22:21 +0800

    change model to suit robot loging model

--- general_tree.py
+++ general_tree.py
@@ -1,35 +1,39 @@
 class Node(object):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, data_s, data_c, data_e):
+        self.data_s = data_s
+        self.data_c = data_c
+        self.data_e = data_e
         self.children = []
 
     def add_child(self, obj):
         self.children.append(obj)
 
 def post_order(Node, children):
+    print(Node.data_s)
     if not children:
+        print(Node.data_c)
         return
     for c in children:
         post_order(c, c.children)
-        print(c.data)
+        print(c.data_e)
 
-n = Node(1)
-p = Node(1.1)
-q = Node(1.2)
-q1 = Node(1.21)
-q2 = Node(1.22)
-q21 = Node(1.221)
-q22 = Node(1.222)
+n = Node('1s','','1e')
+p = Node('1.1s','','1.1e')
+q = Node('1.2s','','1.2e')
+q1 = Node('1.21s','','1.21e')
+q2 = Node('1.22s','','1.22e')
+q21 = Node('1.221s','1.221c','1.221e')
+q22 = Node('1.222s','','1.222e')
 q2.add_child(q21)
 q2.add_child(q22)
 
-q3 = Node(1.22)
+q3 = Node('1.23s','1.23c','1.23e')
 q.add_child(q1)
 q.add_child(q2)
 q.add_child(q3)
 
-p1 = Node(1.11)
-p2 = Node(1.12)
+p1 = Node('1.11s','','1.11e')
+p2 = Node('1.12s','','1.12e')
 p.add_child(p1)
 p.add_child(p2)
 n.add_child(p)
