15108adcac69f36ce83fec034f3065e7ccb5ad0b
Li Ziqiang <ziqiang.li@nokia.com>
2016-06-28 12:21:09 +0800

    add cache decorator

--- general_tree.py
+++ general_tree.py
@@ -1,4 +1,23 @@
+
+class memoized(object):
+   '''Decorator. Caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned
+   (not reevaluated).
+   '''
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      if args in self.cache:
+         return self.cache[args]
+      else:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+
+@memoized
 class Node(object):
+    # __metaclass__ = Singleton
     def __init__(self, data_s, data_c, data_e):
         self.data_s = data_s
         self.data_c = data_c
@@ -19,6 +38,8 @@ def post_order(Node, children):
 
 n = Node('1s','','1e')
 p = Node('1.1s','','1.1e')
+pn = Node('1.1s','','1.1e')
+assert p==pn
 q = Node('1.2s','','1.2e')
 q1 = Node('1.21s','','1.21e')
 q2 = Node('1.22s','','1.22e')
